9abfce0f323dfb7284349a160e9bc4f70b400880
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-03-26 15:12:15 -0300

    setando mongodb

--- pages/api/lib/mongodb.js
+++ pages/api/lib/mongodb.js
@@ -1,7 +1,33 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if(!MONGODB_URI){
-    throw new Error('sem variavel do banco de dados.')
+if (!MONGODB_URI) {
+  throw new Error('Por favor, defina a variável de ambiente MONGODB_URI');
 }
+
+// Check if we are in development mode to cache the connection
+let cached = global.mongoose || { conn: null, promise: null };
+
+async function dbConnect() {
+  if (typeof window !== 'undefined') {
+    throw new Error('dbConnect foi chamado no lado do cliente!');
+  }
+
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI).then((mongooseInstance) => {
+      return mongooseInstance;
+    });
+  }
+
+  cached.conn = await cached.promise;
+// Atrasar a chamada ao cron job para garantir que a conexão esteja pronta
+
+  return cached.conn;
+}
+
+export default dbConnect;
