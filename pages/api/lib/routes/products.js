233e1dfc3db9d0f223057ca2ae20d74ef046ee6d
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-03-28 15:20:14 -0300

    criando rotas

--- pages/api/lib/routes/products.js
+++ pages/api/lib/routes/products.js
@@ -1,3 +1,4 @@
+import product from "../models/product";
 import dbConnect from "../mongodb";
 
 export default async function handler(req, res){
@@ -5,6 +6,14 @@ export default async function handler(req, res){
 
     if(req.method === "GET"){
         const products = await product.find({});
+        return res.status(200).json(products)
+    }
+
+    if(req.method === "POST"){
+        const { name, price} = req.body;
+        const newProduct = new product({name, price});
+        await newProduct.save();
+        return res.status(201).json(newProduct);
     }
 
 }
