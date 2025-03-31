9208b6bb23849b5f09dca2ec38a670664b59cb10
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-03-31 16:33:43 -0300

    criando rotas

--- pages/api/lib/routes/products.js
+++ pages/api/lib/routes/products.js
@@ -1,19 +1,25 @@
-import product from "../models/product";
+import Product from "../models/product";
 import dbConnect from "../mongodb";
 
 export default async function handler(req, res){
     await dbConnect();
 
     if(req.method === "GET"){
-        const products = await product.find({});
+        const products = await Product.find({});
         return res.status(200).json(products)
     }
 
     if(req.method === "POST"){
         const { name, price} = req.body;
-        const newProduct = new product({name, price});
+        const newProduct = new Product({name, price});
         await newProduct.save();
         return res.status(201).json(newProduct);
+    }
+
+    if(req.method === "DELETE"){
+        const { id } = req.body;
+        await Product.findByIdAndDelete(id);
+        return res.status(200).json({ message: "Prduto removido"})
     }
 
 }
