a98f0b55f183da46514a745590d6ce6d4a39681f
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-04-07 16:24:29 -0300

    criando cformulario

--- components/ProductForm.jsx
+++ components/ProductForm.jsx
@@ -1,4 +1,16 @@
+import { useState } from "react";
+
 const ProductForm = () => {
+    const [name, setName] = useState("")
+    const [price, setPrice] = useState("")
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await fetch(`/api/products`, {
+            method: "POST",
+            headers: {"Content-Type": ""}
+        })
+    }
 
 }
 
