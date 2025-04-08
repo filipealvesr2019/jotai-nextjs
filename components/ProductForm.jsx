6aa6972a145c46d30f3bc51bf376d0b08c64f7ec
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-04-08 15:12:43 -0300

    criando cformulario

--- components/ProductForm.jsx
+++ components/ProductForm.jsx
@@ -8,10 +8,17 @@ const ProductForm = () => {
         e.preventDefault();
         await fetch(`/api/products`, {
             method: "POST",
-            headers: {"Content-Type": ""}
-        })
+            headers: {"Content-Type": "aplication/json"},
+            body: JSON.stringify({name, price: Number(price)})
+        }),
+        window.location.reload();
     }
 
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Nome" required/>
+        </form>
+    )
 }
 
 export default ProductForm;
