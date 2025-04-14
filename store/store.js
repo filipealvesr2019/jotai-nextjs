847e4be82c05463f2004917bf5263879b1265ba5
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-04-14 15:10:51 -0300

    criando lista de produtos

--- store/store.js
+++ store/store.js
@@ -1,3 +1,5 @@
 import { atom } from "jotai";
 
 export const productsAtom = atom([]);
+export const cartAtom = atom([]); // array de produtos
+export const cartCountAtom = atom((get) => get(cartAtom).length) // comtador de produtos no carrinho
