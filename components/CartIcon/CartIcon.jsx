27f0657ff0efb1b69e2488e221c7029debf222b3
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-04-15 16:22:09 -0300

    criando cart icon

---
+++ components/CartIcon/CartIcon.jsx
@@ -0,0 +1,14 @@
+import useAtom from "jotai"
+import { cartCountAtom } from "../../store/store"
+
+const CartIcon = () => {
+    const [cartCount] = useAtom(cartCountAtom);
+
+    return (
+        <Link href="/carrinho">
+        <div>
+        🛒
+        </div>
+        </Link>
+    )
+}
