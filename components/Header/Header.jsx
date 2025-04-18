2d5ce71eddfd38a5080acbd5f38adce7815cc7a3
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-04-18 16:41:20 -0300

    criando cart icon

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,10 +1,16 @@
 import CartIcon from "../CartIcon/CartIcon";
-
+import styles from './Header.module.css'
 export default function Header(){
     return (
-        <header>
+        <header className={styles.container}>
+            <section className={styles.section}>
             <span>Logo</span>
+            <div>
             <CartIcon />
+
+            </div>
+
+            </section>
         </header>
     )
 }
