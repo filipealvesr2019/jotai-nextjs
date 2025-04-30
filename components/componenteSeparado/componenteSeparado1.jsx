ffabb4f2750b7e6d0d57b0470fea63fc4bd3289e
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-04-30 16:07:22 -0300

    adiciona estado em componentes isolados

--- components/componenteSeparado/componenteSeparado1.jsx
+++ components/componenteSeparado/componenteSeparado1.jsx
@@ -2,27 +2,27 @@ import { useAtom } from "jotai";
 import { productsAtom } from "../../store/store";
 
 const ComponenteSeparado1 = () => {
+  const [products] = useAtom(productsAtom);
 
-    const [products] = useAtom(productsAtom)
-   
-    return (
-       <div>
-
-        <h1>Produtos em outro componente</h1>
-        <ul style={{
-            display: "flex",
-            flexDirection: "column"
-        }}>
-            {products.map((product) => {
-                return (
-                    <li>
-{product.name} - R${product.price}
-                    </li>
-                )
-            })}
-        </ul>
-       </div>
-    )
-}
+  return (
+    <div>
+      <h1>Produtos em outro componente 1</h1>
+      <ul
+        style={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        {products.map((product) => {
+          return (
+            <li>
+              {product.name} - R${product.price}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
 export default ComponenteSeparado1;
--- components/componenteSeparado/componenteSeparado2.jsx
+++ components/componenteSeparado/componenteSeparado2.jsx
@@ -1,9 +1,24 @@
+import { useAtom } from "jotai";
+import { productsAtom } from "../../store/store";
 
 const ComponenteSeparado2 = () => {
+    const [products] = useAtom(productsAtom)
    
     return (
        <div>
-
+      <h1>Produtos em outro componente 2</h1>
+      <ul style={{
+        display: "flex",
+        flexDirection: "column"
+      }}>
+        {products.map((product) => {
+            return (
+                <li>
+                    {product.name}-R${product.price}
+                </li>
+            )
+        })}
+      </ul>
        </div>
     )
 }
