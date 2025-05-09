efd7760241b4eca9560d625adb6b5bb549f8b02f
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-05-09 16:19:47 -0300

    add

--- store/store.js
+++ store/store.js
@@ -1,5 +1,19 @@
 import { atom } from "jotai";
 
 export const productsAtom = atom([]);
-export const cartAtom = atom([]); // array de produtos
+
+const localStorageCartAtom = atom(() => {
+    if(typeof window !== "undefined"){
+        const saved = localStorage.getItem("cart");
+        return saved ? JSON.parse(saved) : [];
+    }
+    return [];
+})
+export const cartAtom = atom(
+    (get) => get(localStorageCartAtom),
+    (get, set, update) => {
+        const 
+    }
+)
+// export const cartAtom = atom([]); // array de produtos
 export const cartCountAtom = atom((get) => get(cartAtom).length) // comtador de produtos no carrinho
